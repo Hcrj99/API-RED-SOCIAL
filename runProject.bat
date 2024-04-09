5bfff1f652ce2a918e31c2b5a4ce3eeba5d9aa79
hcrj99 <hcrj99@hotmail.com>
2024-04-08 19:45:40 -0500

    connect with database + script to run project .bat

---
+++ runProject.bat
@@ -0,0 +1,4 @@
+start cmd.exe /k "cd C:\Program Files\MongoDB\server\7.0\bin && mongod.exe -dbpath D:/data/db"
+start cmd.exe /k "cd C:\Users\USUARIO\Desktop\Desarrollo\FULL_STACK\APIS\API_REST_REDS && npm start"
+start C:\Users\USUARIO\AppData\Local\MongoDBCompass\MongoDBCompass.exe
+start C:\Users\USUARIO\AppData\Local\Postman\Postman.exe
