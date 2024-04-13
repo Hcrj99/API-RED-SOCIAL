89a337fd17a402208bce94c19c1692e264cd8d20
hcrj99 <hcrj99@hotmail.com>
2024-04-13 10:10:28 -0500

    initial routes + upgrades

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-const connectionDB = require('./DB/ConnectionDB');//Conect with Data Base
+const connectionDB = require('./DB/ConnectionDB');//Connect with Data Base
 const express = require('express');//Import express
 const cors = require('cors');//Import cors
 
 console.log("-----API NODE RED SOCIAL IS RUN------");
-
-//Connect DB
+//!------CONNECT WITH BASE DATA------
 connectionDB();
-//Create Node Server
+
+//!------CREATE NODE SERVER WITH EXPRESS-----
 const app = express();
 const serverPort = 3900;
 //configure cors
@@ -14,7 +14,8 @@ app.use(cors());
 //convert data of body to objects js
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));//every data formurlcode convert in object js
-//create routes
+
+//!-------MAIN ROUTES-------------
 app.get('/test', (req, res) => {
     return res.status(200).json({
         'id':1,
@@ -22,7 +23,7 @@ app.get('/test', (req, res) => {
     })
 })
 
-//server listen
+//!-------RUN SERVER BACKEND-------
 app.listen(serverPort, () => {
     console.log(`-> Red social API Server is RUN in port: ${serverPort}`);
 })
